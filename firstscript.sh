486d045239f73417c024f8c1f1c8e04289e2f94f
Kevin Mason <krmason2020@gmail.com>
2023-09-18 13:22:49 -0700

    .

---
+++ firstscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### this script prints sys info ###
+echo "hello world"
+echo "##############################"
+echo "The uptime of the system is: "
+uptime
+echo "##############################"
+echo "Memory Utilization: "
+free -m
+echo "##############################"
+echo "Disk utilization"
+df -h
